3d0bc4a925171bc93d686c5f1238f2a6018a8b11
Krltx <83478855+Krltx@users.noreply.github.com>
2025-10-02 14:45:22 -0300

    UPDATE 10 25

---
+++ @SOURCES/SPI-10.25.00/ConfigMonitorExtend.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Cambia la configuración de pantalla para extender la pantalla al segundo monitor
+DisplaySwitch /extend
--- Petrinovic/Database/Config.bat
+++
@@ -1,5 +0,0 @@
-@echo Configurando Base de Datos
-@echo off
-sqlcmd -Q "CREATE LOGIN Petrinovic WITH PASSWORD=N'Bartolo', DEFAULT_DATABASE=master, CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF EXEC master..sp_addsrvrolemember @loginame = N'Petrinovic', @rolename = N'sysadmin' RESTORE DATABASE PetrinovicATS FROM DISK='C:\Petrinovic\Database\DataBase.dat' WITH  FILE = 1,  MOVE N'PetrinovicATS_dat' TO N'c:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\PetrinovicATS.mdf',  MOVE N'PetrinovicATS_log' TO N'c:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\PetrinovicATS_1.ldf',  NOUNLOAD,  REPLACE,  STATS = 10"
-@echo Base de datos Creada
-pause
